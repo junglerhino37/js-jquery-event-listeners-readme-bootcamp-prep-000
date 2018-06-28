da13e4037ad827928236dc68bfcb735a885f73e6
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-28 16:58:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
+function getIt(){
+  $('p').on('click',
+  function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+$('img').on('click',function(){
+  $('img').addClass('tasty')
+})
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+
+function frameIt(){
+$('img').on('click',function(){
+  $('img').addClass('tasty')
+})
+}
+
 
 });
